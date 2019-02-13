e86391189a15934b3d24e0f27316d9a6fd90eeb3
Sami MAKKI <smakki@altair.com>
2019-02-13 16:10:30 +0100

    Update pyyaml to 4.2b1 for security reasons

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ def setup_pkr():
             'jinja2',
             'netifaces==0.10.5',
             'pathlib2==2.3.0',
+            'pyyaml>=4.2b1',
             'stevedore==1.21.0'
         ],
         packages=find_packages(exclude=['test', 'docs']),
